815eab260707f2f18f13a8b89bc6f178a4e73676
darade.sudarshan <darade.sudarshan@gmail.com>
2025-09-30 15:10:21 +0530

    added ansible installation script

---
+++ install-ansible.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+echo "[INFO] Detecting OS..."
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+    VERSION_ID=$VERSION_ID
+else
+    echo "[ERROR] Cannot detect the operating system."
+    exit 1
+fi
+
+install_ansible_debian() {
+    echo "[INFO] Updating package index..."
+    sudo apt update
+
+    echo "[INFO] Installing dependencies..."
+    sudo apt install -y software-properties-common
+
+    echo "[INFO] Adding Ansible PPA..."
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+    echo "[INFO] Installing Ansible..."
+    sudo apt install -y ansible
+}
+
+install_ansible_rhel() {
+    echo "[INFO] Installing EPEL repository..."
+    sudo dnf install -y epel-release
+
+    echo "[INFO] Installing Ansible..."
+    sudo dnf install -y ansible
+}
+
+case "$OS" in
+    ubuntu|debian)
+        install_ansible_debian
+        ;;
+    centos|rhel|rocky|almalinux|fedora)
+        install_ansible_rhel
+        ;;
+    *)
+        echo "[ERROR] Unsupported OS: $OS"
+        exit 1
+        ;;
+esac
+
+echo "[SUCCESS] Ansible has been installed."
+ansible --version
